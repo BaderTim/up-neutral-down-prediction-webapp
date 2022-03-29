01ac1bfedf780e52133f741311545db435149325
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2022-03-29 14:34:46 +0200

    commented out backend interface for now

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const bi = new BackendInterface("http://unexpected42.de");
+        // const bi = new BackendInterface("http://unexpected42.de");
     }
 
 
