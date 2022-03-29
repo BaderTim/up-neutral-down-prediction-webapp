fc1865468b07863fe9bb7a2fe9171cfda3abd399
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2022-03-29 13:41:00 +0200

    initial commit

---
+++ src/components/LatestPredictions.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import SinglePrediction from './SinglePrediction';
+
+export default function LatestPredictions(props) {
+
+    const {predictions, groundTruths, currentPrediction} = props;
+
+    const getTimeString = (timestamp) => {
+        return `${('0' + new Date(timestamp).getHours()).slice(-2)}:${('0' + new Date(timestamp).getMinutes()).slice(-2)}`;
+    }
+
+    const latestStep = Date.now() - (Date.now() % (60 * 5 * 1000));
+    const nextStep = latestStep + (60 * 5 * 1000);
+
+    return (
+        <div style={{display: "flex"}}>
+            {predictions.map((prediction, index) => {
+                let currentStep = latestStep - (60 * 5 * (predictions.length - index - 1) * 1000);
+                return <SinglePrediction 
+                            key={index}
+                            prediction={prediction} 
+                            groundTruth={groundTruths[index]} 
+                            time={getTimeString(currentStep)}
+                        />
+            })}
+
+            <div style={{width: "18rem"}}></div>
+
+            <SinglePrediction 
+                prediction={currentPrediction} 
+                groundTruth={currentPrediction} 
+                time={getTimeString(nextStep)} 
+            />
+        </div>
+    );
+    
+}
